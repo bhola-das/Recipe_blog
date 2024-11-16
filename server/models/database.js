8301ce18df4feff1ba57d4eab0750216b703f749
bhola das <bholadas858@gmail.com>
2024-11-16 22:54:27 +0530

    first commit

---
+++ server/models/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('Connected')
+});
+
+// Models
+require('./Category');
+require('./Recipe');
